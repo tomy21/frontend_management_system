d7c37dd814700360b8986d8058eeb74fb8b30a5c
tomy21 <tomy21.agung@gmail.com>
2024-10-05 22:43:25 +0700

    .

--- src/Utils/apiClient.js
+++ src/Utils/apiClient.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL:
-    'https://service-management-system-2buj63496-tomy21s-projects.vercel.app',
+  baseURL: 'https://service-management-system-iota.vercel.app',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
